71001bc96f883280cb5d16b11c340881fb131d65
sanghwa <norimbo12@gmail.com>
2023-05-11 22:50:31 +0900

    init project

---
+++ src/app/pages/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+export function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>HomePage</title>
+        <meta name="description" content="A Boilerplate application homepage" />
+      </Helmet>
+      <span>자 이제 시작이야</span>
+    </>
+  );
+}
